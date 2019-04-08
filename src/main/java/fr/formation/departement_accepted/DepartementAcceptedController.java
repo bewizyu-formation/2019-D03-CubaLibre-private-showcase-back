e73c816f1fb4fea6feac794720ead253adacb472
Stéphane Loiseau <stephloiseau@free.fr>
2019-04-08 16:34:32 +0200

    Fix a request type

--- src/main/java/fr/formation/departement_accepted/DepartementAcceptedController.java
+++ src/main/java/fr/formation/departement_accepted/DepartementAcceptedController.java
@@ -4,10 +4,7 @@ import fr.formation.artist.Artist;
 import fr.formation.controllers.AbstractController;
 import fr.formation.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,7 +23,7 @@ public class DepartementAcceptedController extends AbstractController {
     /**
      * getArtistsByDepartment.
      */
-    @PutMapping("/")
+    @GetMapping("/")
     public List<Artist> getArtistsByDepartment() {
         User user = getAuthenticatedUser();
         return departementAcceptedService.getArtistByDepartement(Integer.parseInt(user.getCodeDepartment()));
