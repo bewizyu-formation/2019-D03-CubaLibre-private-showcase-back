7f1e693ca6fddfe751554e28db670842c61b7092
Haroun Moatassime <haroun.moatassime@gmail.com>
2019-04-09 10:18:35 +0200

    Exception si pass invalid

--- src/main/java/fr/formation/user/UserController.java
+++ src/main/java/fr/formation/user/UserController.java
@@ -4,10 +4,12 @@ package fr.formation.user;
 import fr.formation.controllers.AbstractController;
 import fr.formation.user.exceptions.InvalidPasswordException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.server.ResponseStatusException;
 
 
 import static fr.formation.security.SecurityConstants.ROLE_USER;
@@ -32,7 +34,9 @@ public class UserController extends AbstractController {
 	    try {
             userService.addNewUser(user, ROLE_USER);
         } catch(InvalidPasswordException e){
-
+			throw new ResponseStatusException(
+					HttpStatus.BAD_REQUEST, "Le mot de passe doit contenir au moins 8 " +
+					"caractères dont une minuscule, une majuscule et un chiffre", e);
         }
 	}
 
