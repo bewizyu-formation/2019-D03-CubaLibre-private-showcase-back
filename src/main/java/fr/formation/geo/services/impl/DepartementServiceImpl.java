dd9fba449ac1af0d6166f7cdc3c2a624720192c3
Haroun Moatassime <haroun.moatassime@gmail.com>
2019-04-11 16:55:36 +0200

    department service renvoie liste de LinkedHashMap (#24)
    * department service renvoie liste de LinkedHashMap
    
    * Departementcontroller

--- src/main/java/fr/formation/geo/services/impl/DepartementServiceImpl.java
+++ src/main/java/fr/formation/geo/services/impl/DepartementServiceImpl.java
@@ -1,7 +1,6 @@
 package fr.formation.geo.services.impl;
 
 import fr.formation.geo.GeoApiConstants;
-import fr.formation.geo.model.Departement;
 import fr.formation.geo.services.DepartementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +8,9 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.transaction.Transactional;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 /**
@@ -18,42 +20,36 @@ import java.util.List;
 @Transactional
 public class DepartementServiceImpl implements DepartementService {
 
-	private RestTemplate restTemplate;
+    private RestTemplate restTemplate;
 
-	/**
-	 * Instantiates a new Departement service.
-	 *
-	 * @param restTemplate the rest template
-	 */
-	@Autowired
-	public DepartementServiceImpl(final RestTemplate restTemplate) {
-		this.restTemplate = restTemplate;
-	}
+    /**
+     * Instantiates a new Departement service.
+     *
+     * @param restTemplate the rest template
+     */
+    @Autowired
+    public DepartementServiceImpl(final RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
-	@Override
-	public List<Departement> getDepartement(String nom) {
-		UriComponentsBuilder builder = UriComponentsBuilder
-				.fromUriString(GeoApiConstants.GEO_API_BASE_URL + GeoApiConstants.RESOURCE_DEPARTEMENT)
-				.queryParam(GeoApiConstants.PARAMS_NOM, nom)
-				.queryParam(GeoApiConstants.PARAMS_FIELDS, GeoApiConstants.COMMUNE_FIELDS_VALUES);
+    @Override
+    public List<LinkedHashMap> getDepartements(String nom) throws UnsupportedEncodingException {
+        UriComponentsBuilder builder = UriComponentsBuilder
+                .fromUriString(GeoApiConstants.GEO_API_BASE_URL + GeoApiConstants.RESOURCE_DEPARTEMENT)
+                .queryParam(GeoApiConstants.PARAMS_NOM, nom)
+                .queryParam(GeoApiConstants.PARAMS_FIELDS, GeoApiConstants.COMMUNE_FIELDS_VALUES);
 
-		return this.restTemplate.getForObject(
-				builder.toUriString(),
-				List.class
-		);
-	}
+        return this.restTemplate.getForObject(URLDecoder.decode(builder.toUriString(), "UTF-8"), List.class);
+    }
 
-	@Override
-	public List<Departement> getDepartementByCode(String code) {
-		UriComponentsBuilder builder = UriComponentsBuilder
-				.fromUriString(GeoApiConstants.GEO_API_BASE_URL + GeoApiConstants.RESOURCE_DEPARTEMENT)
-				.queryParam(GeoApiConstants.PARAMS_CODE, code)
-				.queryParam(GeoApiConstants.PARAMS_FIELDS, GeoApiConstants.DEPARTEMENT_FIELDS_VALUES);
+    @Override
+    public List<LinkedHashMap> getDepartementByCode(String code) throws UnsupportedEncodingException {
+        UriComponentsBuilder builder = UriComponentsBuilder
+                .fromUriString(GeoApiConstants.GEO_API_BASE_URL + GeoApiConstants.RESOURCE_DEPARTEMENT)
+                .queryParam(GeoApiConstants.PARAMS_CODE, code)
+                .queryParam(GeoApiConstants.PARAMS_FIELDS, GeoApiConstants.DEPARTEMENT_FIELDS_VALUES);
 
-		return this.restTemplate.getForObject(
-				builder.toUriString(),
-				List.class
-		);
-	}
+        return this.restTemplate.getForObject(URLDecoder.decode(builder.toUriString(), "UTF-8"), List.class);
+    }
 
 }
