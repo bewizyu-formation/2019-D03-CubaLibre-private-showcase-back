21e6793ad50976e104c9ad8a367f01ddf772a220
Haroun Moatassime <haroun.moatassime@gmail.com>
2019-04-16 10:58:38 +0200

    correction injections circulaires

--- src/main/java/fr/formation/user/UserService.java
+++ src/main/java/fr/formation/user/UserService.java
@@ -5,6 +5,7 @@ import fr.formation.artist.ArtistDTO;
 import fr.formation.artist.ArtistRepository;
 import fr.formation.artist.ArtistService;
 import fr.formation.county_accepted.CountyAcceptedService;
+import fr.formation.event.EventRepository;
 import fr.formation.event.EventService;
 import fr.formation.geo.services.impl.CommuneServiceImpl;
 import fr.formation.geo.services.impl.DepartementServiceImpl;
@@ -40,6 +41,8 @@ public class UserService implements UserDetailsService {
 
     private UserRoleRepository userRoleRepository;
 
+    private EventRepository eventRepository;
+
     private PasswordEncoder passwordEncoder;
 
     private CommuneServiceImpl communeServiceImpl;
@@ -47,8 +50,6 @@ public class UserService implements UserDetailsService {
     private DepartementServiceImpl departementServiceImpl;
 
     private ArtistService artistService;
-
-    private EventService eventService;
 
     private CountyAcceptedService countyAcceptedService;
 
@@ -64,22 +65,21 @@ public class UserService implements UserDetailsService {
     public UserService(UserRepository userRepository,
                        ArtistRepository artistRepository,
                        UserRoleRepository userRoleRepository,
+                       EventRepository eventRepository,
                        PasswordEncoder passwordEncoder,
                        CommuneServiceImpl communeServiceImpl,
                        DepartementServiceImpl departementServiceImpl,
                        ArtistService artistService,
-                       EventService eventService,
                        CountyAcceptedService countyAcceptedService) {
         this.userRepository = userRepository;
         this.artistRepository = artistRepository;
         this.userRoleRepository = userRoleRepository;
+        this.eventRepository = eventRepository;
         this.passwordEncoder = passwordEncoder;
         this.communeServiceImpl = communeServiceImpl;
         this.departementServiceImpl = departementServiceImpl;
         this.countyAcceptedService = countyAcceptedService;
         this.artistService = artistService;
-        this.eventService = eventService;
-
     }
 
     /**
@@ -268,7 +268,7 @@ public class UserService implements UserDetailsService {
             user.setEventInvitatedList(
                     userDTO.getEventIdInvitatedList()
                             .stream()
-                            .map(id -> eventService.findById(id))
+                            .map(id -> eventRepository.findById(id).get())
                             .collect(Collectors.toSet())
             );
         }
@@ -277,7 +277,7 @@ public class UserService implements UserDetailsService {
             user.setEventConfirmedList(
                     userDTO.getEventIdConfirmedList()
                             .stream()
-                            .map(id -> eventService.findById(id))
+                            .map(id -> eventRepository.findById(id).get())
                             .collect(Collectors.toSet())
             );
         }
